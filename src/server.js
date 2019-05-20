dfdaae47b0bd8e597bbcf143df9aac549337ca75
Eric Zhang <ericzhang0108@gmail.com>
2019-05-20 15:58:49 -0400

    bot

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import botkit from 'botkit';
+import dotenv from 'dotenv';
+
+dotenv.config({ silent: true });
 
 // initialize
 const app = express();
@@ -30,6 +34,33 @@ app.use(bodyParser.json());
 // default index route
 app.get('/', (req, res) => {
   res.send('hi');
+});
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM((err) => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
+
+// example hello response
+controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.reply(message, 'Hello there!');
 });
 
 // START THE SERVER
